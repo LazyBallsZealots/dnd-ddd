4b729b88f52df98c7e56ffac90eb01641a041300
ntiany <krolkas@gmail.com>
2020-04-09 11:13:11 +0200

    Add new value object - modifier for characteristics

---
+++ Dnd.Ddd.Model/Character/ValueObjects/Characteristics/Modifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dnd.Ddd.Common.ModelFramework;
+
+namespace Dnd.Ddd.Model.Character.ValueObjects.Characteristics
+{
+    class Modifier : ValueObject<Modifier>
+    {
+        private Modifier(int characteristicLevel)
+        {
+            ModifierValue = characteristicLevel / 2 - 5;
+        }
+
+        public static Modifier FromInteger(int characteristicLevel)
+        {
+            return new Modifier(characteristicLevel);
+        }
+
+        public int ModifierValue { get; }
+
+        protected override bool InternalEquals(Modifier valueObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override int InternalGetHashCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
